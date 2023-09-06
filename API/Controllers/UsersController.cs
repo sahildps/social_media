d614fec08f82dd68dfc5ce3a6be52017f54079eb
sahildps <sahildshetty@gmail.com>
2023-09-06 08:40:34 -0700

    Added entity relationship autoMapper and data seeding

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,32 +1,37 @@
 ﻿using API.Controllers;
-using API.Data;
-using API.Entities;
+using API.DTOs;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
 namespace API.Controller;
 
 [Authorize]
 public class UsersController : BaseApiController
 {
-    private readonly DataContext _context;
-       public UsersController(DataContext context)
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public UsersController(IUserRepository userRepository, IMapper mapper)
    {
-        _context = context;
+        _userRepository = userRepository;
+        _mapper = mapper;
    } 
 
-   [AllowAnonymous]
+   
    [HttpGet]
-   public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
-    var users = await _context.Users.ToListAsync();
-    return users;
+   public async Task <ActionResult<IEnumerable<MemberDto>>> GetUsers(){
+
+     var users = await _userRepository.GetMembersAsync();
+     
+     return Ok(users);
    }
 
-//    [AllowAnonymous]
 
-   [HttpGet("{id}")] // /api/users/2
-   public async Task<ActionResult<AppUser>> GetUsers(int id){
-    return await _context.Users.FindAsync(id);
+
+   [HttpGet("{username}")]
+   public async Task<ActionResult<MemberDto>> GetUsers(string username){
+    return await _userRepository.GetMemberAsync (username);
+    
    }
 }
---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Interfaces;
+
+public interface IUserRepository
+{
+    void Update(AppUser user);
+
+    Task<bool> SaveAllAsync();
+
+    Task<IEnumerable<AppUser>> GetUsersAsync();
+
+    Task<AppUser> GetUserByIdAsync(int id);
+
+    Task<AppUser> GetUsersByUsernameAsync(string username);
+
+    Task<IEnumerable<MemberDto>> GetMembersAsync();
+
+    Task<MemberDto> GetMemberAsync(string username);
+}
