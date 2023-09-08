f25f2a02d90a8568b764d150578e696f723c6c23
sahildps <sahildshetty@gmail.com>
2023-09-08 14:19:43 -0700

    implemented client side rendering  and added loading,edit, form

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using API.Controllers;
+﻿using System.Security.Claims;
+using API.Controllers;
 using API.DTOs;
 using API.Interfaces;
 using AutoMapper;
@@ -33,5 +34,20 @@ public class UsersController : BaseApiController
    public async Task<ActionResult<MemberDto>> GetUsers(string username){
     return await _userRepository.GetMemberAsync (username);
     
+   }
+
+   [HttpPut]
+   public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto){
+    
+    var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    var user = await _userRepository.GetUsersByUsernameAsync(username);
+
+    if(user == null)return NotFound();
+
+    _mapper.Map(memberUpdateDto, user);
+
+    if(await _userRepository.SaveAllAsync()) return NoContent();
+
+    return BadRequest("Failed to update user");
    }
 }
